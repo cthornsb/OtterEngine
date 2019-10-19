f9cefe6d3eae4e37eb02edada4d6789cf499b510
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2019-10-19 15:29:46 -0700

    Added camera movement and rotation
    Added basic camera movement and rotation using the
    keyboard and mouse.
    
    Use the w, a, s, and d keys to move the camera
    forward, left, back, and right respectively.
    
    Hold LMB and move the mouse to rotate the camera.

--- source/renderer.cpp
+++ source/renderer.cpp
@@ -14,37 +14,38 @@
 int main(){
 	// Define a new cube
 	cube myCube(vector3(), 1, 1, 1);
-
+	
 	// Set the render mode for our cube
 	myCube.setDrawingMode(scene::SOLID); // Currently, draw options [WIREFRAME, MESH, SOLID, RENDER] are supported
 	
-	// Setup the camera at z=-5 m
-	camera cam(vector3(0, 0, -1));
+	// Setup the camera at z=-1.5 m (facing the cube)
+	camera cam(vector3(0, 0, -1.5));
 	
 	// Show some info about the camera
 	cam.dump();
 	
 	// Setup the scene with our camera
 	scene myScene(&cam);
-
+	//myScene.getWorldLight()->setColor(Colors::RED);
+	
 	// Set the camera to draw surface normal vectors
 	//myScene.setDrawNormals();
 	myScene.setDrawOrigin();
-
+	
 	// Add the cube to the scene
 	myScene.addObject(&myCube);
-
-	// "Animate" the cube by rotating it and moving the camera
+	
+	// "Animate the cube by rotating it and moving the camera
 	int count = 0;
 	bool isDone = false;
 	while(!isDone && myScene.update()){
 		double t = myScene.getTimeElapsed();
-
+		
 		// Check if a key was pressed
 		if(myScene.getKeypress()->down){
 			//std::cout << " key pressed: " << myScene.getKeypress()->key << std::endl;
 			switch(myScene.getKeypress()->key){
-				case 0x1B: 
+				case 0x1B:
 					// Escape key
 					isDone = true;
 					break;
@@ -71,24 +72,28 @@ int main(){
 				case 'x':
 					// Move the camera up
 					cam.moveUp(0.001*t);
+					break;
 				default:
 					break;
 			}
 		}
-	
+		
 		// Check the mouse
 		if(myScene.getMouse()->down){
+			cam.rotate(myScene.getMouse()->xrel*0.01, 0, -myScene.getMouse()->yrel*0.01);
+			myScene.getMouse()->xrel = 0;
+			myScene.getMouse()->yrel = 0;
 		}
-	
-		if(count++ % 100 == 0)
-			std::cout << myScene.getFramerate() << " fps\r" << std::flush;
 		
+		if(count++ % 100 == 0) // Frame count
+			std::cout << myScene.getFramerate() << " fps\r" << std::flush;
+			
 		// Rotate the cube
-		myCube.rotate(0.24*deg2rad, 0.14*deg2rad, 0.34*deg2rad);
+		//myCube.rotate(0.24*deg2rad, 0.14*deg2rad, 0.34*deg2rad);
 		
 		// Move the cube along the x-axis
-		myCube.setPosition(vector3(2*std::sin(0.25*t), 2*std::cos(0.25*t), 0));
-
+		//myCube.setPosition(vector3(2*std::sin(0.25*t), 2*std::cos(0.25*t), 0));
+		
 		// Point the camera at the cube
 		//cam.lookAt(myCube.getPosition());
 	}
