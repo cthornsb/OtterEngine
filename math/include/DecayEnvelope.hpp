cf2f838c62263125139e7e3e077a98fa656325f2
Cory R. Thornsberry <desertedotter@gmail.com>
2021-05-30 11:08:22 -0700

    DecayEnvelope class now behaves as ADSR envelope
    Added attack, decay, and sustain to DecayEnvelope class, following
    the ADSR envelope format.
    
    Added exponential, parabolic, linear, and delta envelope release
    decay types to DecayEnvelope class.

--- math/include/DecayEnvelope.hpp
+++ math/include/DecayEnvelope.hpp
@@ -6,34 +6,55 @@
 namespace ott{
 	class DecayEnvelope{
 	public:
-		/** Default constructor
-		  */
+		enum class DecayType{
+			EXPONENTIAL,
+			PARABOLIC,
+			LINEAR,
+			DELTA
+		};
+	
 		DecayEnvelope() :
 			bActive(false),
-			fCurrent(0.f),
+			bHeld(false),
+			fSustain(0.f),
 			fValue(0.f),
 			fMin(0.f),
-			fAcceleration(1.f),
-			dTotalTime(0)
+			fMax(1.f),
+			fAttackSlope(0.f),
+			fDecaySlope(0.f),
+			fReleaseSlope(0.f),
+			fTimeAttack(1.f),
+			fTimeDecay(1.f),
+			fTimeSustain(-1.f),
+			fTimeSustainStart(2.f),
+			dTotalTime(0),
+			eType(DecayType::EXPONENTIAL)
 		{
 		}
-		
-		/** Initial value constructor
-		  */
-		DecayEnvelope(const float& value, const float& minValue) :
+
+		DecayEnvelope(const float& value, const float& attackTime, const float& decayTime, const float& minValue = 0.f, const float& maxValue = 1.f) :
 			bActive(false),
-			fCurrent(value),
+			bHeld(false),
+			fSustain(value),
 			fValue(value),
 			fMin(minValue),
-			fAcceleration(1.f),
-			dTotalTime(0)
+			fMax(maxValue),
+			fAttackSlope(0.f),
+			fDecaySlope(0.f),
+			fReleaseSlope(0.f),
+			fTimeAttack(attackTime),
+			fTimeDecay(decayTime),
+			fTimeSustain(-1.f),
+			fTimeSustainStart(attackTime + decayTime),
+			dTotalTime(0),
+			eType(DecayType::EXPONENTIAL)
 		{
 		}
-		
+
 		/*float operator () const {
 			return fValue;
 		}*/
-		
+
 		/** Get the current envelope value
 		  */
 		float get() const {
@@ -46,32 +67,83 @@ namespace ott{
 			return bActive;
 		}
 		
-		/** Set "acceleration" of decay (units / s^2)
+		/** Set the "slope" of envelope release phase
 		  */
-		void setAcceleration(const float& acceleration){
-			fAcceleration = acceleration;
+		void setReleaseSlope(const float& slope){
+			fReleaseSlope = slope;
 		}
 		
+		/** Set type of envelope decay
+		  */
+		void setDecayType(const DecayType& type){
+			eType = type;
+		}
+		
+		/** Set the amount of time taken for envelope to rise from minimum output to sustain output value
+		  */
+		void setAttackTime(const double& dt){
+			fTimeAttack = dt;
+			fTimeSustainStart = fTimeAttack + fTimeDecay;
+		}
+		
+		/** Set the amount of time taken for envelope to decay to sustain level from maximum
+		  */
+		void setDecayTime(const double& dt){
+			fTimeDecay = dt;
+			fTimeSustainStart = fTimeAttack + fTimeDecay;
+		}
+
 		/** Set the current driver signal level
 		  */
-		void setDriverSignal(const float& val);
+		void trigger(const float& val);
 		
+		/** Release envelope sustain and start decaying to zero
+		  */
+		void release();
+	
 		/** Update the current envelope time step
 		  */
 		void update(const double& dTime);
+		
+		/** Kill envelope output
+		  */
+		void kill();
 	
-	private:
+	protected:
 		bool bActive; ///< Set if envelope is currently decreasing
+		
+		bool bHeld; ///< Set if sustain is being held, envelope will begin release mode after sustain is disabled
 	
-		float fCurrent; ///< Current value of driver signal
+		float fSustain; ///< Current value of driver signal
 
 		float fValue; ///< Current envelope value
 
 		float fMin; ///< Minimum envelope value
 		
-		float fAcceleration; ///< Envelope decay acceleration (units / s^2)
+		float fMax; ///< Maximum envelope value
+
+		float fAttackSlope; ///< Envelope attack phase slope
+
+		float fDecaySlope; ///< Envelope decay phase slope
+		
+		float fReleaseSlope; ///< Envelope release phase slope
+		
+		double fTimeAttack; ///< Length of time taken for envelope to rise to output level from zero
+		
+		double fTimeDecay; ///< Length of time taken for envelope to decay to sustain level from maximum
+		
+		double fTimeSustain; ///< Length of time that envelope output is held at sustain level
+		
+		double fTimeSustainStart; ///< Time at which envelope begins to decay to zero
 		
 		double dTotalTime; ///< Time since the last driver signal was applied to the envelope
+		
+		DecayType eType; ///< Envelope decay type
+		
+		/** 
+		  */
+		virtual void onUserUpdate() {
+		}
 	};
 }
 
--- math/source/DecayEnvelope.cpp
+++ math/source/DecayEnvelope.cpp
@@ -1,24 +1,70 @@
+#include <cmath>
+
 #include "DecayEnvelope.hpp"
 
 namespace ott{
-	void DecayEnvelope::setDriverSignal(const float& val){
-		fCurrent = val;
-		if(val > fValue){ // Instantaneous impulse
-			fValue = val; 
+	void DecayEnvelope::trigger(const float& val){
+		if(val > fValue){
+			fSustain = val;
 			dTotalTime = 0;
+			fAttackSlope = (fMax - fValue) / fTimeAttack;
+			fDecaySlope = (fSustain - fMax) / fTimeDecay;
 		}
 		bActive = true;
+		bHeld = true;
 	}
 	
+	void DecayEnvelope::release(){
+		dTotalTime = 0;
+		bHeld = false;
+	}
+
 	void DecayEnvelope::update(const double& dTime){
 		if(!bActive)
 			return;
 		dTotalTime += dTime;
-		fValue -= fAcceleration * dTotalTime * dTotalTime;
-		if(fValue <= fMin){
-			fValue = fMin;
-			bActive = false;
+		if(bHeld){ // Attack, decay, sustain
+			if(dTotalTime < fTimeAttack){ // Attack
+				fValue += fAttackSlope * dTime;
+			}
+			else if(dTotalTime < fTimeSustainStart){ // Decay
+				fValue = fMax + fDecaySlope * (dTotalTime - fTimeAttack);
+			}
+			else{ // Sustain
+				fValue = fSustain;			
+			}
 		}
+		else{ // Release
+			switch(eType){ // Update envelope output value
+			case DecayType::EXPONENTIAL:
+				fValue = fMin + fSustain * std::exp(-fReleaseSlope * dTotalTime);
+				if(fValue - fMin <= 1E-6)
+					kill(); // Cease output
+				break;
+			case DecayType::PARABOLIC:
+				fValue = fSustain - fReleaseSlope * dTotalTime * dTotalTime; 
+				if(fValue <= fMin)
+					kill(); // Cease output
+				break;
+			case DecayType::LINEAR: // Constant decrease from sustain output to minimum output
+				fValue = fSustain - fReleaseSlope * dTotalTime;
+				if(fValue <= fMin)
+					kill(); // Cease output
+				break;
+			case DecayType::DELTA: // Instantaneous drop to minimum envelope output
+				kill();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	void DecayEnvelope::kill(){
+		fSustain = 0.f;
+		fValue = fMin; 
+		dTotalTime = 0;
+		bActive = false;
 	}
 }
 
